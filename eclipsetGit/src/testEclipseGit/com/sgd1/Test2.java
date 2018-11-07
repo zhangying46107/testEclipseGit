c128812ad47457fcf6cc0e0bc9118693052f2050
zhangying46107 <zhangying46107@163.com>
2018-11-07 14:40:50 +0800

    Update Test2.java
    fix zhushi

--- eclipsetGit/src/testEclipseGit/com/sgd1/Test2.java
+++ eclipsetGit/src/testEclipseGit/com/sgd1/Test2.java
@@ -3,7 +3,7 @@ package testEclipseGit.com.sgd1;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//hahahahah
 		System.out.println("this is second test!!!");
 
 	}
